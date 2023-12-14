27cb1a78d523cc69cb494a508cabf96e9839806d
AlexNg <taboez4life@gmail.com>
2023-12-14 20:06:34 +0800

    - Removed tests

--- src/tests/homepage.test.ts
+++
@@ -1,9 +0,0 @@
-import HomePage from '@/app/page'
-import { render, screen } from '@testing-library/react'
-
-test('testing', () => {
-  render(HomePage)
-
-  const button = screen.getByText('a')
-  expect(button).toBeDefined()
-})
