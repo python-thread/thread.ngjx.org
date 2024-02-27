4fe9eb5b92c6259308bb4fa7b1ff2f77be9f946a
AlexNg <taboez4life@gmail.com>
2024-02-27 15:39:40 +0800

    feat: Add SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 import type { DocsThemeConfig } from 'nextra-theme-docs'
 
 
@@ -12,11 +14,52 @@ const Footer = (
     </div>
   </div>
 )
+const Head = () => {
+  const { title } = useConfig()
+  const { route } = useRouter()
+
+  const socialImage = `https://thread.ngjx.org/api/og${route === '/' || !title ? '' : `?title=${title}`}`
+
+  return (
+    <>
+      <meta name='msapplication-TileColor' content='#fff' />
+      <meta name='theme-color' content='#fff' />
+
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta httpEquiv='Content-Language' content='en' />
+
+      {/* Icons */}
+      <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+      <link rel='icon' href='/favicon.png' type='image/png' />
+      <link rel='icon' href='/favicon-dark.svg' type='image/svg+xml' media='(prefers-color-scheme: dark)' />
+      <link rel='icon' href='/favicon-dark.png' type='image/png' media='(prefers-color-scheme: dark)' />
+
+      {/* Description */}
+      <meta name='description' content='Your python threading solution ♡⸜(˶˃ ᵕ ˂˶)⸝♡' />
+      <meta name='og:description' content='Your python threading solution ♡⸜(˶˃ ᵕ ˂˶)⸝♡' />
+
+      {/* OpenGraph */}
+      <meta name='og:image' content={socialImage} />
+      <meta name='og:title' content={title ? title + ' - thread' : 'thread'} />
+
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:image' content={socialImage} />
+      <meta name='twitter:site:domain' content='thread.ngjx.org' />
+      <meta name='twitter:url' content='https://thread.ngjx.org' />
+
+      {/* Apple */}
+      <meta name='apple-mobile-web-app-title' content='thread' />
+
+    </>
+  )
+}
 
 
 const config: DocsThemeConfig = {
   logo: Logo,
   head: Head,
+  footer: { text: Footer },
   project: {
     link: 'https://github.com/python-thread/thread'
   },
