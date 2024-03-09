f9213103478c4940eb433d23e46014e68fa7b0fc
AlexNg <taboez4life@gmail.com>
2024-03-09 10:43:19 +0800

    chore: Add svg favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -41,8 +41,10 @@ const Head = () => {
       {/* Icons */}
       <link rel='icon' href='/favicon.png' type='image/png' />
       <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+      <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
       <link rel='icon' href='/favicon-dark.png' type='image/png' media='(prefers-color-scheme: dark)' />
       <link rel='icon' href='/favicon-dark.ico' type='image/x-icon' media='(prefers-color-scheme: dark)' />
+      <link rel='icon' href='/favicon-dark.svg' type='image/svg+xml' media='(prefers-color-scheme: dark)' />
 
       {/* Description */}
       <meta name='description' content='Your python threading solution ♡⸜(˶˃ ᵕ ˂˶)⸝♡' />
