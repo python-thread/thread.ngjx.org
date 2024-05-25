31518e1bc0da99adcab191a4bd22062e20642f84
AlexNg <contact@ngjx.org>
2024-05-25 21:04:20 +0800

    chore(ver): Bump v2.0.1 -> v2.0.2
    Ref: https://github.com/python-thread/thread/pull/87

--- theme.config.tsx
+++ theme.config.tsx
@@ -28,7 +28,7 @@ const Head = () => {
   const { title } = useConfig()
   const { route } = useRouter()
 
-  const pageTitle = route === '/' || !title ? 'Thread v2.0.1' : `${title} | Thread v2.0.1`
+  const pageTitle = route === '/' || !title ? 'Thread v2.0.2' : `${title} | Thread v2.0.2`
   const socialImage = 'https://thread.ngjx.org/socialcard-white.jpg'
 
   return (
@@ -84,7 +84,7 @@ const config: DocsThemeConfig = {
     const { asPath } = useRouter()
     if (asPath !== '/') {
       return {
-        titleTemplate: '%s | Thread v2.0.1',
+        titleTemplate: '%s | Thread v2.0.2',
       }
     }
   },
